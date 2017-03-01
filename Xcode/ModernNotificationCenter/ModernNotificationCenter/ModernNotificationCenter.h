214c1ab5f89c8b3d925a12e900c4e2527bd32464
Alexander Belyavskiy <diejmon@gmail.com>
2017-03-01 17:39:36 +0300

    Remove old Xcode project and replace with correct one.
    macOS target removed from Xcode project for now

---
+++ Xcode/ModernNotificationCenter/ModernNotificationCenter/ModernNotificationCenter.h
@@ -0,0 +1,19 @@
+//
+//  ModernNotificationCenter.h
+//  ModernNotificationCenter
+//
+//  Created by Alexander Belyavskiy on 3/1/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModernNotificationCenter.
+FOUNDATION_EXPORT double ModernNotificationCenterVersionNumber;
+
+//! Project version string for ModernNotificationCenter.
+FOUNDATION_EXPORT const unsigned char ModernNotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModernNotificationCenter/PublicHeader.h>
+
+
