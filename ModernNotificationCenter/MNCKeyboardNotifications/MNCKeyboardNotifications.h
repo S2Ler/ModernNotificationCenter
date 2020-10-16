dfbebdf97ca9916ff7de95dbfbad859800caa6da
Aliaksandr Bialiauski <Aliaksandr@Bialiauski.com>
2020-10-16 20:38:37 +0300

    Xcode project

---
+++ ModernNotificationCenter/MNCKeyboardNotifications/MNCKeyboardNotifications.h
@@ -0,0 +1,13 @@
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MNCKeyboardNotifications.
+FOUNDATION_EXPORT double MNCKeyboardNotificationsVersionNumber;
+
+//! Project version string for MNCKeyboardNotifications.
+FOUNDATION_EXPORT const unsigned char MNCKeyboardNotificationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MNCKeyboardNotifications/PublicHeader.h>
+
+
---
+++ ModernNotificationCenter/ModernNotificationCenter/ModernNotificationCenter.h
@@ -0,0 +1,13 @@
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModernNotificationCenter.
+FOUNDATION_EXPORT double ModernNotificationCenterVersionNumber;
+
+//! Project version string for ModernNotificationCenter.
+FOUNDATION_EXPORT const unsigned char ModernNotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModernNotificationCenter/PublicHeader.h>
+
+
